e766714ff43d6bcd65244c77c9118f1eed580a69
RAO YANG <lightrao@hotmail.com>
2019-10-06 15:26:10 +0800

    add lesson

---
+++ 02_PythonStrings/02_exmaple.py
@@ -0,0 +1,22 @@
+message = "Meet me tonight."
+print(message)
+
+message2 = "The clock strikes at midnight."
+print(message2)
+
+message3 = "I'am looking for someone to share in an adventure"
+print(message3)
+
+message4 = "I'am looking for someone to share in an adventure"
+print(message4)
+
+message5 = 'The phrase "Beam me up, Scotty." was never said on Star Trek.'
+print(message5)
+
+movie_quote = \
+"""
+One of my favorite lines from The Godfather is:
+"I'm going to make him an offer he can't refuse"
+Do you know who said this?
+"""
+print(movie_quote)
