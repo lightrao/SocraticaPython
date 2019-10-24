4a703abdfa0334d0c34b4b4e6dd6d386acaed7fb
lightrao <lightrao@hotmail.com>
2019-10-24 13:24:55 +0800

    update

---
+++ 25_SortinginPython/25_example.py
@@ -0,0 +1,11 @@
+# Alkaline earth metals
+
+earth_metals = ["Berylium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium"]
+print(earth_metals)
+earth_metals.sort()
+print(earth_metals)
+earth_metals.sort(reverse=True)
+print(earth_metals)
+
+earth_metals = ("Berylium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium")
+# earth_metals.sort()
---
+++ 25_SortinginPython/25_example3.py
@@ -0,0 +1,12 @@
+earth_metals = ["Berylium", "Magnesium", "Calcium", "Strontium", "Barium", "Radium"]
+sorted_earth_mentals = sorted(earth_metals)
+print(sorted_earth_mentals)
+print(earth_metals)
+
+data = (7, 2, 5, 6, 1, 3, 9, 10, 4, 8)
+sorted_data = sorted(data)
+print(sorted_data)
+print(data)
+
+print(sorted("Alphabetical"))
+
